a7d84156284df95e862ea8320dfee88fd10d89d1
Mohan Sai <tellakula.mohan28sai@gmail.com>
2023-08-20 23:45:04 +0530

    changes made to routes.cpp

--- routes.cpp
+++ routes.cpp
@@ -8,11 +8,11 @@ crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     int sum = input["first"].i()+input["second"].i();
-    return crow::response{"{\"result\":"+to_string(sum)+"}"};
+    return crow::response{"{\"result\":"+std::__cxx11::to_string(sum)+"}"};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     int sub = input["first"].i()-input["second"].i();
-    return crow::response{"{\"result\":"+to_string(sub)+"}"};
+    return crow::response{"{\"result\":"+std::__cxx11::to_string(sub)+"}"};
 }
