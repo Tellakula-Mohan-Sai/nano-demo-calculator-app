eba2d1549e4dc2202eac5fdcd851d9dd4bb3ef40
Mohan Sai <tellakula.mohan28sai@gmail.com>
2023-08-20 23:32:31 +0530

    changes made to routes.cpp

--- routes.cpp
+++ routes.cpp
@@ -7,12 +7,12 @@ crow::response greet()
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    int sum = input['first']+input['second'];
+    int sum = input["first"]+input["second"];
     return crow::response{"result":sum};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    int sub = input['first']-input['second'];
+    int sub = input["first"]-input["second"];
     return crow::response{"result":sub};
 }
